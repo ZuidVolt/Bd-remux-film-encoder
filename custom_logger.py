b54bab7c20a85a9a3ad0d40fab092fd224b6a3b4
Ether717 <95562180+Ether717@users.noreply.github.com>
2024-10-28 11:27:45 +0800

    added custom logger system back with file output and terminal output

--- custom_logger.py
+++ custom_logger.py
@@ -1,15 +1,30 @@
 import logging
 import subprocess
 import time
+import sys
+from datetime import datetime
 
 
 class CustomLogger(logging.Logger):
     def __init__(self, name):
         super().__init__(name)
         self.setLevel(logging.INFO)
-        self.handler = logging.StreamHandler()
-        self.handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-        self.addHandler(self.handler)
+        self.addHandler(logging.StreamHandler(sys.stdout))
+        file_handler = logging.FileHandler(f"encoding_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log", encoding="utf-8")
+        file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+        self.addHandler(file_handler)
+
+    def info(self, msg, *args, **kwargs):
+        super().info(msg, *args, **kwargs)
+        self.handlers[-1].flush()
+
+    def warning(self, msg, *args, **kwargs):
+        super().warning(msg, *args, **kwargs)
+        self.handlers[-1].flush()
+
+    def error(self, msg, *args, **kwargs):
+        super().error(msg, *args, **kwargs)
+        self.handlers[-1].flush()
 
     def log_input_analysis(self, probe_data):
         video_stream = next((s for s in probe_data["streams"] if s["codec_type"] == "video"), None)
